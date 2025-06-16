8167ba58ae61e7f498cab1042a979941a234b241
joãoA <joaovictor.dudu03@gmail.com>
2025-06-15 21:11:56 -0300

    inicio do projeto. Estilização quase pronta, html feito(sujeito a alterações) e js incompleto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-alert('ola mundo')
+const inputNp1 = document.getElementById('inputNp1')
+const inputNp2 = document.getElementById('inputNp2')
+const InputPim = document.getElementById('inputPim')
+const btnCalcular = document.getElementById('btnCalcular')
+
+
+btnCalcular.addEventListener('click', function(){
+
+    np1 = inputNp1.value
+    np2 = inputNp2.value    
+    pim = inputPim.value
+
+    if(isNaN(np1) || isNaN(np2) || isNaN(pim)){
+        let erro = document.getElementById('erro')
+        erro.style.display = 'block'
+        return
+    }
+
+    media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2)
+    mostrarMedia(media)
+
+})
+
+function mostrarMedia(media){
+    let mediaF = document.getElementById('mediaF')
+    mediaF.style.display = 'block'
+    mediaF.textContent = `Média FInal: ${media.toFixed(2)}`
+}
+
