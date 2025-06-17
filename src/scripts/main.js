84012a258fe96653a61cbd24f5e7cd54fbd4e1bd
joãoA <joaovictor.dudu03@gmail.com>
2025-06-16 21:41:41 -0300

    projeto funcional. apenas estilizar o APROVADO e REPROVADO

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,51 @@
 const inputNp1 = document.getElementById('inputNp1')
 const inputNp2 = document.getElementById('inputNp2')
-const InputPim = document.getElementById('inputPim')
+const inputPim = document.getElementById('inputPim')
 const btnCalcular = document.getElementById('btnCalcular')
 
+const arrayInput = [inputNp1, inputNp2, inputPim]
+
+arrayInput.forEach(function(element){
+    element.addEventListener('input', verificaInput)
+})
 
 btnCalcular.addEventListener('click', function(){
+    
+    const np1 = parseFloat(inputNp1.value)
+    const np2 = parseFloat(inputNp2.value)
+    const pim = parseFloat(inputPim.value)
+    
+    // if(isNaN(np1) || isNaN(np2) || isNaN(pim)){
+    //     let erro = document.getElementById('erro')
+    //     erro.style.display = 'block'
+    //     return
+    // }
 
-    np1 = inputNp1.value
-    np2 = inputNp2.value    
-    pim = inputPim.value
-
-    if(isNaN(np1) || isNaN(np2) || isNaN(pim)){
-        let erro = document.getElementById('erro')
-        erro.style.display = 'block'
-        return
-    }
-
-    media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2)
+    const media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2)
     mostrarMedia(media)
-
 })
 
 function mostrarMedia(media){
     let mediaF = document.getElementById('mediaF')
     mediaF.style.display = 'block'
-    mediaF.textContent = `Média FInal: ${media.toFixed(2)}`
+    const status = media >= 7 ? "Aprovado" : "Reprovado";
+    mediaF.textContent = `Média Final: ${media.toFixed(2)} - ${status}`
 }
+
+function verificaInput(){
+    const np1 = parseFloat(inputNp1.value)
+    const np2 = parseFloat(inputNp2.value)
+    const pim = parseFloat(inputPim.value)
+
+    if(!isNaN(np1) && !isNaN(np2) && !isNaN(pim)){
+        btnCalcular.disabled = false
+        btnCalcular.classList.add('active');
+        btnCalcular.classList.add('active:hover')
+    } else {
+        btnCalcular.disabled = true;
+        btnCalcular.classList.remove('active');
+    }
+}
+
+
 
